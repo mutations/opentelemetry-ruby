49fe3df9dc12d3dfe038876fa5e8d8dc03c52ec6
David Davis <david.davis@mutations.ltd>
2019-12-17 10:23:51 -0800

    Add **/adapter.rb

---
+++ adapters/action_pack/lib/opentelemetry/adapters/action_pack/adapter.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Copyright 2019 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+module OpenTelemetry
+  module Adapters
+    module ActionPack
+      class Adapter
+        class << self
+          def install(config = {})
+          end
+        end
+
+        def install
+        end
+      end
+    end
+  end
+end
