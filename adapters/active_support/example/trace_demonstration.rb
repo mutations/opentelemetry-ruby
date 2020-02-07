9b7f10fa1b86124170b82afcccd6ad906909b065
David Davis <david.davis@mutations.ltd>
2020-02-06 18:10:48 -0800

    WIP: Add example: trace_demonstration

---
+++ adapters/active_support/example/trace_demonstration.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Copyright 2020 OpenTelemetry Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+require 'rubygems'
+require 'bundler/setup'
+
+Bundler.require
+
+OpenTelemetry::SDK.configure do |c|
+  c.use 'OpenTelemetry::Adapters::ActiveSupport'
+end
+
+# demonstrate tracing (span output to console):
